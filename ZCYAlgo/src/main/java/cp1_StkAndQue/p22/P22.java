b1a1efe687201d341ca3d096df00756dcebd1d9f
Lenny-Zh <lenny.z@foxmail.com>
2017-08-16 20:16:25 +0800

    add p22

---
+++ ZCYAlgo/src/main/java/cp1_StkAndQue/p22/P22.java
@@ -0,0 +1,17 @@
+package cp1_StkAndQue.p22;
+
+/**
+ * Created by Administrator on 2017/3/29 0029.
+ * MaxTree定义如下:
+ * 1. 数组必须没有重复元素
+ * 2. MaxTree是一棵二叉树,数组的每一个值对应一个二叉树节点
+ * 3. 包括MaxTree树在内且在其中的的每一棵子树上,值最大的节点都是树的头
+ * 给一个数组arr,写一个数组转换MaxTree函数,数组长度为N
+ * 要求: 时间复杂度为O(N),额外空间复杂度为O(N)
+ *
+ * 难度 : 校 ***
+ */
+public class P22 {
+
+
+}
