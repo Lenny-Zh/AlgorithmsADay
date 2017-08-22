f4094ddc0ce1d31cd8361d48c32e94eaf7e6c9ad
Lenny-Zh <lenny.z@foxmail.com>
2017-08-22 20:45:55 +0800

    update p35 from book

---
+++ ZCYAlgo/src/main/java/cp2_LinkedListQuestion/p36/P36.java
@@ -0,0 +1,23 @@
+package cp2_LinkedListQuestion.p36;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/22 0022.
+ * 删除链表的中间节点和a/b处节点
+ * 1. 给定链表, 删除中间节点, 如果中间有两个节点删除第一个,如果有一个删除它
+ * 2. 给定链表,整数a和b, 删除位于a/b
+ *
+ * 难度: 士 * (1级)
+ */
+public class P36 {
+
+    @Test
+    public void testP35(){
+
+
+    }
+
+
+
+}
