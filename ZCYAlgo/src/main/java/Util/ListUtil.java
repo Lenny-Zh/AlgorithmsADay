3bfe22d3d16436a678b4d01f21b2fb93042dc235
Jarly <jarly.zhong@outlook.com>
2017-08-23 21:32:22 +0800

    add test vector

--- ZCYAlgo/src/main/java/Util/ListUtil.java
+++ ZCYAlgo/src/main/java/Util/ListUtil.java
@@ -2,6 +2,7 @@ package Util;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Vector;
 
 /**
  * Created by Administrator on 2017/8/18 0018.
@@ -35,6 +36,21 @@ public class ListUtil {
         list.add(9);
         return list;
     }
+
+    public static Vector<Integer> getTestVectorSort(){
+        Vector<Integer> list = new Vector<Integer>(10);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        return list;
+    }
+
 
 
 }
